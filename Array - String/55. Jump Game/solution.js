6c81b3ddaa15f92b5304f4ed23fa994423d734c2
rathiharvir <rathiharvir@gmail.com>
2024-03-05 22:41:40 +0530

    55. Jump Game

---
+++ Array - String/55. Jump Game/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let lastPosition = nums.length - 1;
+
+    for (let i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= lastPosition) {
+            lastPosition = i;
+        }
+    }
+
+    return lastPosition === 0; 
+};
